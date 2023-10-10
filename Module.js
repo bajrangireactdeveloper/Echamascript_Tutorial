1ae7d10a299a7f59f2ff4cc3ef16dbb1eaafe9df
Bajrangi <bajrangilal6198@gmail.com>
2023-10-10 23:29:43 +0530

    Echama Script Tutorial

---
+++ Module.js
@@ -0,0 +1,11 @@
+export default function Module(){
+    return "Hello Import";
+}
+
+// export default function OtherFunc(){
+//     return "Hello Other Module Function";
+// }
+
+export function newFunc(){
+    return "New Function";
+}
